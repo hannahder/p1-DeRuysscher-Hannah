e190195546bdcde0dfcf9da16853ce259cd2ee34
Hannah De Ruysscher <hderuysscher@ufl.edu>
2020-10-27 08:09:26 -0400

    Changed time of nav

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,14 +3,14 @@ $( document ).ready(function() {
 $( ".cross" ).hide();
 $( ".menu" ).hide();
 $( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
+$( ".menu" ).slideToggle( "400", function() {
 $( ".hamburger" ).hide();
 $( ".cross" ).show();
 });
 });
 
 $( ".cross" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
+$( ".menu" ).slideToggle( "400", function() {
 $( ".cross" ).hide();
 $( ".hamburger" ).show();
 });
